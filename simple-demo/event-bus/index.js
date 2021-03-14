6a83dadc602bb3fca7502d961a18dac133adbd15
sk2111 <sathisht1996@gmail.com>
2021-03-14 21:43:13 +0530

    Add : Event store pattern of all events
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- simple-demo/event-bus/index.js
+++ simple-demo/event-bus/index.js
@@ -4,17 +4,35 @@ const app = express();
 
 app.use(express.json());
 
+
+const events = [];
+
+
+
+
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
+    events.push(push);
 
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log(err.message);
+    });
     res.send({ status: 'OK' });
 });
 
+app.get('/events', (req, res) => {
+    res.send(events);
+});
 
 app.listen(4005, () => {
     console.log("event bus lisitining on 4005");
--- simple-demo/query/index.js
+++ simple-demo/query/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
@@ -13,10 +14,7 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/events', (req, res) => {
-
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
         posts[id] = { id, title, comments: [] };
@@ -37,12 +35,21 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
-    console.log(posts);
-
+}
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    handleEvent(type, data);
     res.send({});
 });
 
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log("listening on port 4002");
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+        console.log('Processing event', event.type);
+        handleEvent(event.type, event.data);
+    }
 });
