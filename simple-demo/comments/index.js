27d712aa212105d5afc1e0af5da89550533c4a26
sk2111 <sathisht1996@gmail.com>
2021-03-14 11:23:20 +0530

    Add : React base project setup
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

---
+++ simple-demo/comments/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const { randomBytes } = require('crypto');
+
+const app = express();
+
+const commmentsByPostId = {};
+
+app.use(express.json());
+
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commmentsByPostId[req.params.id] ?? []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commmentsByPostId[req.params.id] ?? [];
+    comments.push({ id: commentId, content });
+
+    commmentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen('4001', () => {
+    console.log("listeneing on 4001");
+})
